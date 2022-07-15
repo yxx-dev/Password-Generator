40b1d0501275f3a2f5cee7ec40a74d488ecd13fc
yxx-dev <hermitlun@gmail.com>
2022-07-15 10:11:18 -0700

    initial commit

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,28 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.getElementById("password");
+
+  function generatePassword () {
+
+
+    //prompt for password length
+    let length = prompt("Please enter your password length");
+    if (length>=8 && length<=128) {
+
+    } else alert("Invalid password length");
+
+
+  }
+
 
   passwordText.value = password;
-
+  return;
 }
 
 // Add event listener to generate button
